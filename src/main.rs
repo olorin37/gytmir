fd5c2863b3e7a1741337f3ac693cae10e98f19a5
Jakub A. Gramsz <jakub.gramsz@gmail.com>
2025-01-24 22:58:35 +0100

    Move whole crate logic to another lib one
    I want to have ability to use those function in different frames, first
    is CLI, next will be simple http server which will invoke its functions
    while serving http requiest e.g. to terminate WebHooks

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,5 @@
-use git2::Repository;
-use git2::{Cred, RemoteCallbacks, FetchOptions, PushOptions};
-use std::env;
 use clap::Parser;
+use gytmir_internal::sync;
 
 #[derive(Parser, Debug)]
 #[command(
@@ -9,6 +7,7 @@ use clap::Parser;
     about,
     long_about = None
 )]
+
 struct Cli {
     #[arg(short = None, long, default_value = "../doxtractor")]
     /// Path to repository directory
@@ -25,64 +24,6 @@ struct Cli {
     #[arg(short = None, long, default_value = ".ssh/id_rsa")]
     /// Set ssh key
     key_file: String,
-}
-
-fn credentials_remote_callbacks(key_file: &str) -> RemoteCallbacks {
-    let mut callbacks = RemoteCallbacks::new();
-    callbacks.credentials(move |_url, username_from_url, _allowed_types | {
-      Cred::ssh_key(
-        username_from_url.unwrap(),
-        None,
-        std::path::Path::new(
-            &format!("{}/{}", env::var("HOME").unwrap(), key_file)
-        ),
-        None,
-      )
-    });
-
-    callbacks
-}
-
-fn credentials_in_fetch_options(key_file: &str) -> FetchOptions {
-    let mut fetch_options = FetchOptions::new();
-    fetch_options.remote_callbacks(credentials_remote_callbacks(key_file));
-
-    fetch_options
-}
-
-fn credentials_in_push_options(key_file: &str) -> PushOptions {
-    let mut push_options = PushOptions::new();
-    push_options.remote_callbacks(credentials_remote_callbacks(key_file));
-
-    push_options
-}
-
-fn sync(
-    repo_dir: &str,
-    remote: &str,
-    key_file: &str,
-    branch: &str,
-) {
-    let repo = Repository::open(repo_dir).unwrap();
-    println!("{:?}", repo.head().unwrap().name().unwrap());
-    for remote in repo.remotes().unwrap().iter() {
-        println!("{:?}", remote);
-    }
-    let mut remote = repo.find_remote(&remote).unwrap();
-    for refspec in remote.fetch_refspecs().unwrap().iter() {
-        println!("{:?}", refspec);
-    }
-
-    let mut fetch_options = credentials_in_fetch_options(&key_file);
-
-    let () = remote.fetch(&[&branch], Some(&mut fetch_options), None).unwrap();
-
-    let mut mirror_remote = repo.remote("mirror", "git@gitlab.com:olorin37/doxtractor.git").unwrap();
-
-    let mut push_options = credentials_in_push_options(&key_file);
-    let _ = mirror_remote.push(&["refs/remotes/origin/master"], Some(&mut push_options)).unwrap();
-
-    println!("Mirror remote: {:?}", mirror_remote.url().unwrap());
 }
 
 fn main() {

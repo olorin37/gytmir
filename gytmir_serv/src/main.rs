9ee095c668953c86a630b1936dca5f5caf3fedd2
Jakub A. Gramsz <jakub.gramsz@gmail.com>
2025-01-25 23:20:11 +0100

    Add http server for triggering gytmir
    I want to allow invoke gytmir commands to be triggered by web hooks

---
+++ gytmir_serv/src/main.rs
@@ -0,0 +1,25 @@
+use actix_web::{get, web, App, HttpServer, HttpResponse, Responder};
+use gytmir_internal::sync;
+use std::thread;
+
+#[get("/")]
+async fn index() -> impl Responder {
+    thread::spawn(|| {
+        sync("../../doxtractor", "origin", ".ssh/id_rsa", "master");
+    });
+    HttpResponse::Accepted()
+
+}        
+
+#[get("/{name}")]
+async fn hello(name: web::Path<String>) -> impl Responder {
+    format!("Hello {}!\n", &name)
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(index).service(hello))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
+}
